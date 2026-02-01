8e9f3f1ccf4f66c7d9c532c6100c8b9e422aeb2f
bhavnachauhann <bhavnac297@gmail.com>
2026-02-02 00:01:06 +0530

    first commit

--- firebase/client.ts
+++ firebase/client.ts
@@ -8,17 +8,45 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+//   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+//   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+//   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+//   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+//   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+// };
+
+// // Initialize Firebase
+// const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+// // const analytics = getAnalytics(app);
+
+// export const auth = getAuth(app);
+// export const db = getFirestore(app);
+
+// my code
+// Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyBDn3aEDrJ8sJxMMyWM0c4cCQTX7Ufz2yg",
+  authDomain: "prepwise-47685.firebaseapp.com",
+  projectId: "prepwise-47685",
+  storageBucket: "prepwise-47685.firebasestorage.app",
+  messagingSenderId: "434586156285",
+  appId: "1:434586156285:web:497ec021ffb7f236a39294",
+  measurementId: "G-4DL3G37QPY"
 };
 
 // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 // const analytics = getAnalytics(app);
 
